93f3a8e340fbdae663957c83e3dec3e7d7c08130
Habidhossen <habidhossen2@gmail.com>
2023-09-16 04:02:17 +0600

    modal updated

--- models/book.model.js
+++ models/book.model.js
@@ -18,6 +18,10 @@ const bookSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    userEmail: {
+      type: String,
+      required: true,
+    },
     coverImage: {
       type: String,
     },
